e8b425f9d7b4101c0088f6526661dff2f92407cf
nicholas evans <nicholas.evans@gmail.com>
2023-01-28 14:04:15 -0500

    🚧 RFC4466: add tagged-ext-val, etc (used by STATUS, ESEARCH, LIST, etc)

--- lib/net/imap/response_parser.rb
+++ lib/net/imap/response_parser.rb
@@ -447,6 +447,52 @@ module Net
         NIL? ? nil : case_insensitive__string
       end
 
+      # tagged-ext-comp     = astring /
+      #                       tagged-ext-comp *(SP tagged-ext-comp) /
+      #                       "(" tagged-ext-comp ")"
+      #                       ; Extensions that follow this general
+      #                       ; syntax should use nstring instead of
+      #                       ; astring when appropriate in the context
+      #                       ; of the extension.
+      #                       ; Note that a message set or a "number"
+      #                       ; can always be represented as an "atom".
+      #                       ; A URL should be represented as
+      #                       ; a "quoted" string.
+      def tagged_ext_comp
+        vals = []
+        while true
+          case lookahead!(*ASTRING_TOKENS, LPAR).symbol
+          when T_LPAR
+            lpar; vals << tagged_ext_comp; rpar
+          when T_NUMBER
+            vals << number # special-case: returns an Integer, not a string
+          else
+            vals << astring
+          end
+          SP? or break
+        end
+        vals
+      end
+
+      # tagged-ext-simple is a subset of atom
+      # TODO: recognize sequence-set in the lexer
+      #
+      # tagged-ext-simple   = sequence-set / number / number64
+      def tagged_ext_simple
+        case lookahead!(*ATOM_TOKENS).symbol
+        when T_NUMBER then number
+        else               sequence_set
+        end
+      end
+
+      # tagged-ext-val      = tagged-ext-simple /
+      #                       "(" [tagged-ext-comp] ")"
+      def tagged_ext_val
+        if lpar? then _ = tagged_ext_comp; rpar; _
+        else              tagged_ext_simple
+        end
+      end
+
       # See https://www.rfc-editor.org/errata/rfc3501
       #
       # charset = atom / quoted
